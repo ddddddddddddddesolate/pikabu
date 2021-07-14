4900235aa4ba0cd998e7899553c823f817253514
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-14 18:05:57 +0600

    updated login user mutation

--- app/graphql/mutations/auth/login_user.rb
+++ app/graphql/mutations/auth/login_user.rb
@@ -3,18 +3,15 @@ module Mutations
     class LoginUser < BaseMutation
       null true
 
-      argument :credentials, Types::Auth::AuthProviderCredentialsInput, required: false
+      argument :credentials, Types::Auth::AuthProviderCredentialsInput, required: true
 
       field :token, String, null: true
       field :user, Types::UserType, null: true
 
       def resolve(credentials: nil)
-        nil unless credentials
+        user = User.find_by!(email: credentials[:email])
 
-        user = User.find_by email: credentials[:email]
-
-        nil unless user
-        nil unless user.authenticate credentials[:password]
+        user.authenticate(credentials[:password])
 
         crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
         token = crypt.encrypt_and_sign("user:#{user.id}")
