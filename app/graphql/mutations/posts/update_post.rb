d9f2e14993e7e973bd19e36084ec3c0527580e21
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-16 15:27:34 +0600

    created update post mutation

---
+++ app/graphql/mutations/posts/update_post.rb
@@ -0,0 +1,26 @@
+module Mutations
+  module Posts
+    class UpdatePost < Mutations::BaseMutation
+      argument :id, ID, required: true
+      argument :title, String, required: false
+      argument :text, String, required: false
+
+      field :post, Types::PostType, null: true
+      field :errors, [String], null: true
+
+      def resolve(id:, title:, text: nil)
+        raise GraphQL::ExecutionError, 'You need to authenticate to perform this action' unless context[:current_user]
+
+        post = context[:current_user].posts.find_by(id: id)
+
+        raise GraphQL::ExecutionError, 'Post not found' unless post.present?
+
+        if post.update(title: title, text: text)
+          { post: post }
+        else
+          { errors: post.errors.full_messages }
+        end
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :login_user, mutation: Mutations::Auth::LoginUser
     field :logout_user, mutation: Mutations::Auth::LogoutUser
     field :create_post, mutation: Mutations::Posts::CreatePost
+    field :update_post, mutation: Mutations::Posts::UpdatePost
     field :create_tag, mutation: Mutations::Tags::CreateTag
   end
 end
