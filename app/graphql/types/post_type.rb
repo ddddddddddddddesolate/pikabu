b1380b160e7b33b49d7cd6641752f9f8ac3cdb4e
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-16 19:05:08 +0600

    fixed post type

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,8 +4,6 @@ module Types
     field :user, UserType, null: false
     field :title, String, null: false
     field :text, String, null: true
-    field :tags, [TagType], null: true do
-      argument :attributes, TagAttributes
-    end
+    field :tags, [TagType], null: true
   end
 end
