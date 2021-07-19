c88c6955c7ae855c3d121bb012a19d340234c7fb
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 15:33:23 +0600

    implemented edit comment mutation

---
+++ app/graphql/mutations/comments/edit_comment.rb
@@ -0,0 +1,21 @@
+module Mutations
+  module Comments
+    class EditComment < BaseMutation
+      argument :comment_id, ID, required: true
+      argument :text, String, required: true
+
+      field :comment, Types::CommentType, null: true
+      field :errors, [String], null: true
+
+      def resolve(comment_id:, text:)
+        comment = current_user.comments.find(comment_id)
+
+        if comment.update(text: text)
+          { comment: comment }
+        else
+          { errors: comment.errors.full_messages }
+        end
+      end
+    end
+  end
+end
