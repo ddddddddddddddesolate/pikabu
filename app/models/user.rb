6bd90e63f8e5e588136301c6a3759dadb9ce2466
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 19:07:43 +0600

    added votes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :delete_all
+  has_many :votes, dependent: :delete_all
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
