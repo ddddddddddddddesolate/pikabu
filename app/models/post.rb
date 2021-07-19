c8136221713a95f08d53043a1a236ecdff349ecb
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 11:57:35 +0600

    created comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
 
   has_and_belongs_to_many :tags
 
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :text, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_135013) do
+ActiveRecord::Schema.define(version: 2021_07_19_055113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -45,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_07_14_135013) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "posts_tags", "posts"
   add_foreign_key "posts_tags", "tags"
