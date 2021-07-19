908badbde38bfd0c82ac8ac4bc4af9f199e78a55
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 13:05:54 +0600

    added ability to reply to a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  # parent
+  belongs_to :comment, optional: true
+  # replies
+  has_many :comments
+
   validates :text, presence: true, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_055113) do
+ActiveRecord::Schema.define(version: 2021_07_19_065303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_07_19_055113) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "comment_id"
+    t.index ["comment_id"], name: "index_comments_on_comment_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -55,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_055113) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "comments"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
