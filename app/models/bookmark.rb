0b3c3b43d1f0fd1882be16a119182412578cead0
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 16:19:05 +0600

    added uniqueness for bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :bookmarkable, polymorphic: true
+
+  validates :user, uniqueness: { scope: [:bookmarkable_id, :bookmarkable_type] }
 end
