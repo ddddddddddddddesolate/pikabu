052de3ca4873c39636f41d468e733a64c8084f29
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 16:50:52 +0600

    updated validation for bookmarks uniqueness

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :bookmarkable, polymorphic: true
 
-  validates :user, uniqueness: { scope: [:bookmarkable_id, :bookmarkable_type] }
+  validates :bookmarkable_id, uniqueness: { scope: [:bookmarkable_id, :bookmarkable_type],
+                                 message: "object already exists in bookmarks" }
 end
