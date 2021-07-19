1dfd2bf076c0c87d01da13978282e51936047a9e
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-19 15:06:30 +0600

    moved comments method of post type to separate resolver

---
+++ app/graphql/resolvers/post_root_comments_resolver.rb
@@ -0,0 +1,9 @@
+module Resolvers
+  class PostRootCommentsResolver < BaseResolver
+    type Types::CommentType, null: false
+
+    def resolve
+      Comment.where(post_id: object.id, comment_id: nil)
+    end
+  end
+end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,10 +5,6 @@ module Types
     field :title, String, null: false
     field :text, String, null: true
     field :tags, [TagType], null: true
-    field :comments, [CommentType], null: true
-
-    def comments
-      Comment.where(post_id: object.id, comment_id: nil)
-    end
+    field :comments, [CommentType], null: true, resolver: Resolvers::PostRootCommentsResolver
   end
 end
