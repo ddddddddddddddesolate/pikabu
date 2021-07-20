8cf5c9618da7df8fd9260b248b3e832c898e77da
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-20 13:48:16 +0600

    fixed votes for posts

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -9,6 +9,6 @@ module Types
     field :comments_count, Integer, null: false, resolver: Resolvers::CommentsCountResolver
     field :likes_count, Integer, null: false, resolver: Resolvers::LikesCountResolver
     field :dislikes_count, Integer, null: false, resolver: Resolvers::DislikesCountResolver
-    field :votes, VoteType, null: false
+    field :votes, [VoteType], null: false
   end
 end
