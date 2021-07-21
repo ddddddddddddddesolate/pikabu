c368a78e5604f72a52d38abb03573416a55639b2
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-21 16:09:06 +0600

    implemented pagination for comments

--- app/graphql/resolvers/comments_resolver.rb
+++ app/graphql/resolvers/comments_resolver.rb
@@ -3,14 +3,21 @@ module Resolvers
     type [Types::CommentType], null: false
 
     argument :post_id, ID, required: true
-    argument :order, Types::OrderFields, required: false
 
-    def resolve(post_id:, order: nil)
+    argument :order, Types::OrderFields, required: false
+    argument :paginate, Types::Pagination, required: false
+
+    def resolve(post_id:, order: nil, paginate: nil)
       comments = Comment.where(post_id: post_id)
 
       if order
         comments = comments.likes(order.likes) if order.likes
         comments = comments.date(order.date) if order.date
+      end
+
+      if paginate
+        comments = comments.limit(paginate.limit)
+        comments = comments.offset(paginate.offset)
       end
 
       comments
