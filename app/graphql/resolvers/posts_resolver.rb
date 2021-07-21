5c26815d50479134387ad3bfa32f35894ff643d4
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-21 11:45:20 +0600

    implemented posts pagination

--- app/graphql/resolvers/posts_resolver.rb
+++ app/graphql/resolvers/posts_resolver.rb
@@ -6,14 +6,18 @@ module Resolvers
     argument :hot, Boolean, required: false
     argument :best, Boolean, required: false
     argument :date, String, required: false
+    argument :limit, Integer, required: false
+    argument :offset, Integer, required: false
 
-    def resolve(fresh: nil, hot: nil, best: nil, date: nil)
+    def resolve(fresh: nil, hot: nil, best: nil, date: nil, limit: nil, offset: nil)
       posts = Post.all
 
       posts = posts.fresh if fresh
       posts = posts.hot if hot
       posts = posts.best if best
       posts = posts.order(created_at: date) if date
+      posts = posts.limit(limit) if limit
+      posts = posts.offset(offset) if offset
 
       posts
     end
