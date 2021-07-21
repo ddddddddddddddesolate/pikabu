db4f7481e0d2a14bac1f88d3123bcceb94e3cb74
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-21 11:30:16 +0600

    implemented posts order by date

--- app/graphql/resolvers/posts_resolver.rb
+++ app/graphql/resolvers/posts_resolver.rb
@@ -5,13 +5,15 @@ module Resolvers
     argument :fresh, Boolean, required: false
     argument :hot, Boolean, required: false
     argument :best, Boolean, required: false
+    argument :date, String, required: false
 
-    def resolve(fresh: nil, hot: nil, best: nil)
+    def resolve(fresh: nil, hot: nil, best: nil, date: nil)
       posts = Post.all
 
       posts = posts.fresh if fresh
       posts = posts.hot if hot
       posts = posts.best if best
+      posts = posts.order(created_at: date) if date
 
       posts
     end
