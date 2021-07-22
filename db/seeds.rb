dfeb124a20bbbb730a5523e0f9640d966bee02c8
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-22 10:04:54 +0600

    added random creation date generation to seeder, changed amount of all generated objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,32 @@
 require "faker"
 
 # users generator
-30.times do
+10.times do
   User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, password: "123456")
 end
 
 # posts generator
-30.times do
-  post = Post.create(user_id: User.all.sample.id, title: Faker::Lorem.sentence(word_count: 3), text: Faker::Lorem.sentence(word_count: 10))
+20.times do
+  post = Post.create(
+    user_id: User.all.sample.id,
+    title: Faker::Lorem.sentence(word_count: 3),
+    text: Faker::Lorem.sentence(word_count: 10),
+    created_at: rand(1..50).hours.ago
+  )
 
-  rand(1..3).times do
+  rand(0..3).times do
     post.images.create(remote_image_url: "https://file-examples-com.github.io/uploads/2017/10/file_example_JPG_100kB.jpg")
   end
 end
 
 # comments generator
-40.times do
-  comment = Comment.create(post_id: Post.all.sample.id, user_id: User.all.sample.id, text: Faker::Lorem.sentence(word_count: 5))
+150.times do
+  comment = Comment.create(
+    post_id: Post.all.sample.id,
+    user_id: User.all.sample.id,
+    text: Faker::Lorem.sentence(word_count: 5),
+    created_at: rand(1..50).hours.ago
+  )
 
   rand(0..2).times do
     comment.images.create(remote_image_url: "https://file-examples-com.github.io/uploads/2017/10/file_example_JPG_100kB.jpg")
@@ -26,7 +36,15 @@ end
 end
 
 # likes / dislikes generator
-100.times do
-  Post.all.sample.votes.create(reaction: rand(0..1), user_id: User.all.sample.id)
-  Comment.all.sample.votes.create(reaction: rand(0..1), user_id: User.all.sample.id)
+200.times do
+  Post.all.sample.votes.create(
+    reaction: rand(0..1),
+    user_id: User.all.sample.id,
+    created_at: rand(1..50).hours.ago
+  )
+  Comment.all.sample.votes.create(
+    reaction: rand(0..1),
+    user_id: User.all.sample.id,
+    created_at: rand(1..50).hours.ago
+  )
 end
