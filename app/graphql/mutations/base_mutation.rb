c23879cc22a00e28ab37f41d22a90c407f6720d6
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-23 11:56:07 +0600

    removed user checking from base mutation

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -2,10 +2,6 @@ module Mutations
   class BaseMutation < GraphQL::Schema::Mutation
     null false
 
-    def current_user
-      context[:current_user]
-    end
-
     def cookies
       context[:cookies]
     end
