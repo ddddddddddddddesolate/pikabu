a9034297e14f2555568cf18f3de0d96b9fc12020
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-23 11:02:46 +0600

    updated add and remove tag from post errors catching

--- app/graphql/mutations/posts_tags/add_tag_to_post.rb
+++ app/graphql/mutations/posts_tags/add_tag_to_post.rb
@@ -7,8 +7,15 @@ module Mutations
       field :post, Types::PostType, null: false
 
       def resolve(post_id:, tag_id:)
-        post = current_user.posts.find(post_id)
-        tag = Tag.find(tag_id)
+        post = Post.find_by(id: post_id)
+
+        raise GraphQL::ExecutionError, "Post not found" unless post.present?
+        raise GraphQL::ExecutionError, "You cannot tag this post" unless post.user_id == current_user.id
+
+        tag = Tag.find_by(id: tag_id)
+
+        raise GraphQL::ExecutionError, "Tag not found" unless tag.present?
+        raise GraphQL::ExecutionError, "Tag already exists in this post" if post.tags.exists?(tag_id)
 
         post.tags << tag
 
