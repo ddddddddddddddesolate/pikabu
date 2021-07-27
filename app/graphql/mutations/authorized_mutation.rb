2b13a15de348a4904fb44b1e2370ca1a773a4890
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-27 11:47:28 +0600

    updated error handling in authorized mutation

--- app/graphql/mutations/authorized_mutation.rb
+++ app/graphql/mutations/authorized_mutation.rb
@@ -1,7 +1,7 @@
 module Mutations
   class AuthorizedMutation < BaseMutation
     def current_user
-      raise GraphQL::ExecutionError, "You are not authorized to perform this action" unless context[:current_user]
+      raise Exceptions::UnauthorizedError, "You are not authorized to perform this action" unless context[:current_user]
 
       context[:current_user]
     end
