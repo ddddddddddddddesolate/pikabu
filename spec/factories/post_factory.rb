5a39ecc943dd5a99d8c17a9824780b162e4d30df
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-28 16:12:03 +0600

    created post factory

---
+++ spec/factories/post_factory.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'faker'
+
+FactoryBot.define do
+  factory :post do
+    user
+    title { Faker::Lorem.sentence(word_count: 4) }
+    text { Faker::Lorem.sentence(word_count: 10) }
+  end
+end
