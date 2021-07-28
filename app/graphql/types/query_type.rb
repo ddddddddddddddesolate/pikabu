968df585fa082810b1273417b4145cd2696b1215
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-28 11:33:38 +0600

    removed comments query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,7 +6,6 @@ module Types
     include GraphQL::Types::Relay::HasNodesField
 
     field :posts, [PostType], null: true, resolver: Resolvers::PostsResolver
-    field :comments, [CommentType], null: true, resolver: Resolvers::CommentsResolver
     field :bookmarks, [BookmarkType], null: true, resolver: Resolvers::BookmarksResolver
   end
 end
