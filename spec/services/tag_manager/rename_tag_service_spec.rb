6c824bb06c2cf2b0f87b9a28841fc42a308e4260
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 12:01:18 +0600

    added faker to rails helper

--- spec/services/tag_manager/rename_tag_service_spec.rb
+++ spec/services/tag_manager/rename_tag_service_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'faker'
 
 RSpec.describe TagManager::RenameTagService do
   let(:result) { TagManager::RenameTagService.call(id, name) }
