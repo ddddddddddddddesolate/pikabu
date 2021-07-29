8613016c62bc440f96d449243d64be74a698507e
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 12:53:32 +0600

    implemented delete comment service test

---
+++ spec/services/comment_manager/delete_comment_service_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CommentManager::DeleteCommentService do
+  let(:result) { CommentManager::DeleteCommentService.call(current_user, id) }
+
+  let(:current_user) { nil }
+  let(:id) { nil }
+
+  context 'when user not logged in' do
+    it 'raise UnauthorizedError' do
+      expect { result }.to raise_error Exceptions::UnauthorizedError
+    end
+  end
+
+  context 'when user logged in' do
+    let(:current_user) { create(:user) }
+
+    context 'and comment not present' do
+      it 'raise NotFoundError' do
+        expect { result }.to raise_error Exceptions::NotFoundError
+      end
+    end
+
+    context 'and user not author of comment' do
+      let(:id) { create(:comment).id }
+
+      it 'raise NotFoundError' do
+        expect { result }.to raise_error Exceptions::NotFoundError
+      end
+    end
+  end
+end
