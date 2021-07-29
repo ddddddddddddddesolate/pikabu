e63e3ce8bd99e608772c69b89f719725193279b4
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 09:43:17 +0600

    implemented rename tag service test

---
+++ spec/services/tag_manager/rename_tag_service_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'faker'
+
+RSpec.describe TagManager::RenameTagService do
+  describe 'TagManager::RenameTagService' do
+    let :result do
+      TagManager::RenameTagService.call(id, name)
+    end
+
+    context 'where tag not exists' do
+      let :id do
+        0
+      end
+
+      let :name do
+        Faker::Lorem.characters(number: 30)
+      end
+
+      it 'raise NotFoundError' do
+        expect { result }.to raise_error Exceptions::NotFoundError
+      end
+    end
+
+    context 'where tag name is invalid' do
+      let :tag do
+        create :tag
+      end
+
+      let :id do
+        tag.id
+      end
+
+      let :name do
+        nil
+      end
+
+      it 'raise ValidationError' do
+        expect { result }.to raise_error Exceptions::ValidationError
+      end
+    end
+
+    context 'where tag name already taken' do
+      let :first_tag do
+        create :tag
+      end
+
+      let :second_tag do
+        create :tag
+      end
+
+      let :id do
+        first_tag.id
+      end
+
+      let :name do
+        second_tag.name
+      end
+
+      it 'raise ValidationError' do
+        expect { result }.to raise_error Exceptions::ValidationError
+      end
+    end
+  end
+end
