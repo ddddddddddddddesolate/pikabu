eb566546841f804a7b88dbf533158d7f05248678
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 11:13:36 +0600

    fixed update post service parameters

--- app/services/post_manager/update_post_service.rb
+++ app/services/post_manager/update_post_service.rb
@@ -2,24 +2,28 @@
 
 module PostManager
   class UpdatePostService < AuthorizedService
-    attr_reader :id, :attributes
+    attr_reader :id, :title, :text
 
-    def initialize(current_user, id, attributes)
+    def initialize(current_user, id, title, text)
       super(current_user)
 
       @id = id
-      @attributes = attributes
+      @title = title
+      @text = text
     end
 
     def call
       post = current_user.posts.includes(:user, :tags, :images, reactions: [:user]).find(id)
 
-      raise ActiveRecord::RecordInvalid, post unless post.update(attributes.to_h)
+      raise ActiveRecord::RecordInvalid, post unless post.update(
+        title: title,
+        text: text
+      )
 
       post
     rescue ActiveRecord::RecordNotFound
       raise Exceptions::NotFoundError, 'Post not found'
-    rescue ActiveRecord::RecordInvalid, e
+    rescue ActiveRecord::RecordInvalid => e
       raise Exceptions::ValidationError, e.message
     end
   end
