216f74ce2b00458755e926871e0ef54819c403d3
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 11:53:12 +0600

    implemented delete post service test

---
+++ spec/services/post_manager/delete_post_service_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostManager::DeletePostService do
+  let(:result) { PostManager::DeletePostService.call(current_user, id) }
+
+  context 'when user not logged in' do
+    let(:current_user) { nil }
+    let(:id) { nil }
+
+    it 'raise UnauthorizedError' do
+      expect { result }.to raise_error Exceptions::UnauthorizedError
+    end
+  end
+
+  context 'when user logged in' do
+    let(:current_user) { create(:user) }
+
+    context 'and post not exists' do
+      let(:id) { nil }
+
+      it 'raise NotFoundError' do
+        expect { result }.to raise_error Exceptions::NotFoundError
+      end
+    end
+
+    context 'and post exists' do
+      context 'and user not author of post' do
+        let(:other_user) { create(:user) }
+        let(:post) { create(:post, user: other_user) }
+        let(:id) { post.id }
+
+        it 'raise NotFoundError' do
+          expect { result }.to raise_error Exceptions::NotFoundError
+        end
+      end
+    end
+  end
+end
