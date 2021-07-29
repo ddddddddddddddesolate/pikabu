26c6abcab5b8863e246d8ad691ad7143d4539362
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 09:32:33 +0600

    renamed factories, removed after create from post factory, moved factory bot config to separate file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+require 'support/factory_bot'
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -19,6 +21,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-  config.include FactoryBot::Syntax::Methods
 end
