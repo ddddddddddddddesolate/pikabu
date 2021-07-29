150434a388b47cefaf6d7b44e825d2bdcc7a30d5
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-29 10:08:14 +0600

    updated authorized service

--- app/services/authorized_service.rb
+++ app/services/authorized_service.rb
@@ -4,6 +4,8 @@ class AuthorizedService < ApplicationService
   attr_reader :current_user
 
   def initialize(current_user)
+    raise Exceptions::UnauthorizedError unless current_user
+
     @current_user = current_user
   end
 end
