197c6bd3990bd6e1dfed56e25c4410b81be99c57
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-30 18:06:04 +0600

    refactored create post service test

--- spec/services/post_manager/create_post_service_spec.rb
+++ spec/services/post_manager/create_post_service_spec.rb
@@ -1,87 +1,37 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PostManager::CreatePostService do
-  let(:result) { described_class.call(current_user, title, text, image_urls, tag_names) }
+  let(:result) { described_class.call(current_user, attributes, image_urls, tag_names) }
 
-  let(:current_user) { nil }
-  let(:title) { nil }
-  let(:text) { nil }
+  let(:current_user) { create(:user) }
+  let(:attributes) { nil }
   let(:image_urls) { nil }
   let(:tag_names) { nil }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
+  context "when attributes is invalid" do
+    it "result not successful" do
+      expect(result.success).eql? false
     end
   end
 
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
+  context "when attributes is valid" do
+    let(:attributes) { { title: Faker::Lorem.characters(number: 50) } }
 
-    context 'and post has' do
-      context 'blank title' do
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
+    context "and images has invalid urls" do
+      let(:image_urls) { [nil] }
 
-      context 'too long title' do
-        let(:title) { Faker::Lorem.characters(number: 51) }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
-
-      context 'too long text' do
-        let(:title) { Faker::Lorem.characters(number: 50) }
-        let(:text) { Faker::Lorem.characters(number: 256) }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
+      it "result not successful" do
+        expect(result.success).eql? false
       end
     end
 
-    context 'image has' do
-      let(:title) { Faker::Lorem.characters(number: 50) }
+    context "and tags has invalid names" do
+      let(:tag_names) { [nil] }
 
-      context 'invalid url' do
-        let(:image_urls) { ['invalid.url'] }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
-
-      context 'blank url' do
-        let(:image_urls) { [nil] }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
-    end
-
-    context 'tag has' do
-      let(:title) { Faker::Lorem.characters(number: 50) }
-
-      context 'blank name' do
-        let(:tag_names) { [nil] }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
-
-      context 'too long name' do
-        let(:tag_names) { [Faker::Lorem.characters(number: 31)] }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
+      it "result not successful" do
+        expect(result.success).eql? false
       end
     end
   end
