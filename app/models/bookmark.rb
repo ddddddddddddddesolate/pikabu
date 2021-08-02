e6232bc7373a53c2774bfdd9a1cd7a0360f9dad1
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:40:01 +0600

    added validation for bookmarkable type

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class Bookmark < ApplicationRecord
+  ALLOWED_TYPES = %w[Post Comment].freeze
+
   belongs_to :user
   belongs_to :bookmarkable, polymorphic: true
 
   counter_culture :user
 
   validates :user_id, uniqueness: { scope: %w[bookmarkable_id bookmarkable_type] }
+  validates :bookmarkable_type, presence: true, inclusion: { in: ALLOWED_TYPES }
 end
