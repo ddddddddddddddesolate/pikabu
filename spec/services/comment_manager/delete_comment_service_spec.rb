62cf4da4f7509897229694e59eca86020b176080
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:28:26 +0600

    refactored delete comment service test

--- spec/services/comment_manager/delete_comment_service_spec.rb
+++ spec/services/comment_manager/delete_comment_service_spec.rb
@@ -3,32 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe CommentManager::DeleteCommentService do
-  let(:result) { described_class.call(current_user, id) }
+  let(:result) { described_class.call(comment) }
 
-  let(:current_user) { nil }
-  let(:id) { nil }
+  let(:comment) { create(:comment) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
-
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and comment not present' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
-    end
-
-    context 'not author of comment' do
-      let(:id) { create(:comment).id }
-
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
+  context 'when comment exists' do
+    it 'result successful' do
+      expect(result.success).eql? true
     end
   end
 end
