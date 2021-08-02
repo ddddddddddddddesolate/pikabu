6d51a851cd77eb01a4d415b75074871b8e89f0d2
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:23:37 +0600

    refactored update comment service test

--- spec/services/comment_manager/update_comment_service_spec.rb
+++ spec/services/comment_manager/update_comment_service_spec.rb
@@ -3,53 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe CommentManager::UpdateCommentService do
-  let(:result) { described_class.call(current_user, id, text) }
+  let(:result) { described_class.call(comment, params) }
 
-  let(:current_user) { nil }
-  let(:id) { nil }
-  let(:text) { nil }
+  let(:comment) { create(:comment) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
-
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and comment not exists' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
+  context 'when params is invalid' do
+    let(:params) do
+      {
+        text: nil
+      }
     end
 
-    context 'and comment' do
-      context 'author is not current user' do
-        let(:id) { create(:comment).id }
-
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'of current user has' do
-        let(:id) { create(:comment, user: current_user).id }
-
-        context 'blank text' do
-          it 'raise ValidationError' do
-            expect { result }.to raise_error Exceptions::ValidationError
-          end
-        end
-
-        context 'too long text' do
-          let(:text) { Faker::Lorem.characters(number: 256) }
-
-          it 'raise ValidationError' do
-            expect { result }.to raise_error Exceptions::ValidationError
-          end
-        end
-      end
+    it 'result not successful' do
+      expect(result.success).eql? false
     end
   end
 end
