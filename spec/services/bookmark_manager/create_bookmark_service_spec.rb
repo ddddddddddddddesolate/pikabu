08eb68f99e6f7bc84ac9d004c9dfc8c2ae998ab1
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:42:57 +0600

    refactored create bookmark service test

--- spec/services/bookmark_manager/create_bookmark_service_spec.rb
+++ spec/services/bookmark_manager/create_bookmark_service_spec.rb
@@ -3,64 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe BookmarkManager::CreateBookmarkService do
-  let(:result) { described_class.call(current_user, model, id) }
+  let(:result) { described_class.call(current_user, record) }
 
-  let(:current_user) { nil }
-  let(:model) { nil }
-  let(:id) { nil }
+  let(:current_user) { create(:user) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
+  context 'when record is not bookmarkable' do
+    let(:record) { create(:tag) }
 
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and type of bookmark not set' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
-    end
-
-    context 'and post' do
-      let(:model) { Post }
-
-      context 'not exists' do
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'exists in bookmarks' do
-        let(:bookmark) { create(:bookmark, :for_post, user: current_user, bookmarkable: create(:post)) }
-        let(:id) { bookmark.bookmarkable_id }
-
-        it 'raise AlreadyExistsError' do
-          expect { result }.to raise_error Exceptions::AlreadyExistsError
-        end
-      end
-    end
-
-    context 'comment' do
-      let(:model) { Comment }
-
-      context 'not exists' do
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'exists in bookmarks' do
-        let(:bookmark) { create(:bookmark, :for_comment, user: current_user, bookmarkable: create(:comment)) }
-        let(:id) { bookmark.bookmarkable_id }
-
-        it 'raise AlreadyExistsError' do
-          expect { result }.to raise_error Exceptions::AlreadyExistsError
-        end
-      end
+    it 'result not successful' do
+      expect(result.success).eql? false
     end
   end
 end
-
