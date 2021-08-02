6a880735cc5d9278232177eb618bb745e9bedf3d
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 09:55:19 +0600

    renamed attributes to params in create post service test

--- spec/services/post_manager/create_post_service_spec.rb
+++ spec/services/post_manager/create_post_service_spec.rb
@@ -3,21 +3,21 @@
 require "rails_helper"
 
 RSpec.describe PostManager::CreatePostService do
-  let(:result) { described_class.call(current_user, attributes, image_urls, tag_names) }
+  let(:result) { described_class.call(current_user, params, image_urls, tag_names) }
 
   let(:current_user) { create(:user) }
-  let(:attributes) { nil }
+  let(:params) { nil }
   let(:image_urls) { nil }
   let(:tag_names) { nil }
 
-  context "when attributes is invalid" do
+  context "when params is invalid" do
     it "result not successful" do
       expect(result.success).eql? false
     end
   end
 
-  context "when attributes is valid" do
-    let(:attributes) { { title: Faker::Lorem.characters(number: 50) } }
+  context "when params is valid" do
+    let(:params) { { title: Faker::Lorem.characters(number: 50) } }
 
     context "and images has invalid urls" do
       let(:image_urls) { [nil] }
