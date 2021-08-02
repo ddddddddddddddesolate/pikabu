b969e234a6cdd6663c6d2edf60541e91db783934
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:20:08 +0600

    refactored add comment to post service test

--- spec/services/comment_manager/add_comment_to_post_service_spec.rb
+++ spec/services/comment_manager/add_comment_to_post_service_spec.rb
@@ -3,43 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe CommentManager::AddCommentToPostService do
-  let(:result) { described_class.call(current_user, id, text) }
+  let(:result) { described_class.call(current_user, post, params) }
 
-  let(:current_user) { nil }
-  let(:id) { nil }
-  let(:text) { nil }
+  let(:current_user) { create(:user) }
+  let(:post) { create(:post) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
-
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and post not exists' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
+  context 'when params is invalid' do
+    let(:params) do
+      {
+        text: nil
+      }
     end
 
-    context 'and post has' do
-      let(:id) { create(:post).id }
-
-      context 'blank text' do
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
-
-      context 'too long text' do
-        let(:text) { Faker::Lorem.characters(number: 256) }
-
-        it 'raise ValidationError' do
-          expect { result }.to raise_error Exceptions::ValidationError
-        end
-      end
+    it 'result not successful' do
+      expect(result.success).eql? false
     end
   end
 end
