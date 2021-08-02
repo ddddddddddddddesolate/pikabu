aa3391e8f82d743d85ce597ad8c4154e31da9e00
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:45:14 +0600

    refactored remove bookmark service test

--- spec/services/bookmark_manager/remove_bookmark_service_spec.rb
+++ spec/services/bookmark_manager/remove_bookmark_service_spec.rb
@@ -3,61 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe BookmarkManager::RemoveBookmarkService do
-  let(:result) { described_class.call(current_user, model, id) }
+  let(:result) { described_class.call(bookmark) }
 
-  let(:current_user) { nil }
-  let(:model) { nil }
-  let(:id) { nil }
+  context 'when bookmark exists' do
+    let(:bookmark) { create(:bookmark) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
-
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and type of bookmark not set' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
-    end
-
-    context 'and post' do
-      let(:model) { Post }
-
-      context 'not exists' do
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'not exists in bookmarks' do
-        let(:id) { create(:post).id }
-
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-    end
-
-    context 'of type comment' do
-      let(:model) { Comment }
-
-      context 'not exists' do
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'not exists in bookmarks' do
-        let(:id) { create(:comment).id }
-
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
+    it 'result successful' do
+      expect(result.success).eql? true
     end
   end
 end
