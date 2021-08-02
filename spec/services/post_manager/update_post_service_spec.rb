9cba3f9b1530f0708885e009429d35f94c1616e4
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 10:01:31 +0600

    refactored update post service test

--- spec/services/post_manager/update_post_service_spec.rb
+++ spec/services/post_manager/update_post_service_spec.rb
@@ -3,64 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe PostManager::UpdatePostService do
-  let(:result) { described_class.call(current_user, id, title, text) }
+  let(:result) { described_class.call(post, params) }
 
-  let(:current_user) { nil }
-  let(:id) { nil }
-  let(:title) { nil }
-  let(:text) { nil }
+  let(:post) { create(:post) }
 
-  context 'when user not logged in' do
-    it 'raise UnauthorizedError' do
-      expect { result }.to raise_error Exceptions::UnauthorizedError
-    end
-  end
-
-  context 'when user logged in' do
-    let(:current_user) { create(:user) }
-
-    context 'and post not exists' do
-      it 'raise NotFoundError' do
-        expect { result }.to raise_error Exceptions::NotFoundError
-      end
+  context 'when params is invalid' do
+    let(:params) do
+      {
+        title: nil
+      }
     end
 
-    context 'and post' do
-      context 'author is not current user' do
-        let(:id) { create(:post, user: create(:user)).id }
-        let(:title) { Faker::Lorem.characters(number: 50) }
-
-        it 'raise NotFoundError' do
-          expect { result }.to raise_error Exceptions::NotFoundError
-        end
-      end
-
-      context 'of current user has' do
-        let(:id) { create(:post, user: current_user).id }
-
-        context 'blank title' do
-          it 'raise ValidationError' do
-            expect { result }.to raise_error Exceptions::ValidationError
-          end
-        end
-
-        context 'too long title' do
-          let(:title) { Faker::Lorem.characters(number: 51) }
-
-          it 'raise ValidationError' do
-            expect { result }.to raise_error Exceptions::ValidationError
-          end
-        end
-
-        context 'too long text' do
-          let(:title) { Faker::Lorem.characters(number: 50) }
-          let(:text) { Faker::Lorem.characters(number: 256) }
-
-          it 'raise ValidationError' do
-            expect { result }.to raise_error Exceptions::ValidationError
-          end
-        end
-      end
-    end
+    expect(result.success).eql? false
   end
 end
