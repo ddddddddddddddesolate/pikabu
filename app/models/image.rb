a045d22f55d67c5d10cd8af7e85eff04e4df2d49
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-02 11:05:26 +0600

    added validation for imageable types

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class Image < ApplicationRecord
+  ALLOWED_TYPES = %w[Post Comment].freeze
+
   belongs_to :imageable, polymorphic: true
 
   mount_uploader :image, ImageUploader
 
   validates :image, presence: true, allow_blank: false
+  validates :imageable_type, presence: true, inclusion: { in: ALLOWED_TYPES }
 end
