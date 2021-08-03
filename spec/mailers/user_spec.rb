dc286f56ad65d1e7fa9c3eeeebc763c4037ebecf
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 12:55:22 +0600

    implemented test for user mailer

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "send" do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.welcome_email(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).eql? "Welcome to Pikabu!"
+      expect(mail.from).eql? [Rails.application.credentials.mailer[:user_name]]
+      expect(mail.to).eql? [user.email]
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match "Welcome to Pikabu, #{user.name}!"
+    end
+  end
 end
