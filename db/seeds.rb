90ac7d5ec52596beb6555f1af4be0de16486eb2d
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 17:40:40 +0600

    decreased amount of entities generated in seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@
 
 require "faker"
 
-image = File.open(Rails.root.join("data", "sample.jpeg"))
+image = File.open(Rails.root.join("data/sample.jpeg"))
 
 # users generator
-10.times do
+3.times do
   User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, password: "123456")
 end
 
 # posts generator
-50.times do
+5.times do
   post = Post.create(
     user_id: User.all.sample.id,
     title: Faker::Lorem.sentence(word_count: 3),
@@ -19,13 +19,13 @@ end
   )
 
   # post images
-  rand(0..3).times do
+  rand(0..2).times do
     post.images.create(image: image)
   end
 end
 
 # comments generator
-150.times do
+30.times do
   comment = Comment.create(
     post_id: Post.all.sample.id,
     user_id: User.all.sample.id,
@@ -34,28 +34,23 @@ end
   )
 
   # comment images
-  rand(0..2).times do
+  rand(0..1).times do
     comment.images.create(image: image)
   end
 
   # replies
-  rand(0..10).times do
+  rand(0..3).times do
     reply = comment.comments.create(
       post_id: Post.all.sample.id,
       user_id: User.all.sample.id,
       text: Faker::Lorem.sentence(word_count: 5),
       created_at: rand(1..50).hours.ago
     )
-
-    # reply images
-    rand(0..2).times do
-      reply.images.create(image: image)
-    end
   end
 end
 
 # likes / dislikes for posts
-300.times do
+50.times do
   Post.all.sample.reactions.create(
     reaction: rand(0..1),
     user_id: User.all.sample.id,
@@ -64,7 +59,7 @@ end
 end
 
 # likes / dislikes for comments
-1500.times do
+100.times do
   Comment.all.sample.reactions.create(
     reaction: rand(0..1),
     user_id: User.all.sample.id,
@@ -74,7 +69,7 @@ end
 
 # bookmarks
 User.all.each do |user|
-  rand(0..15).times do
+  rand(0..5).times do
     types = [Post, Comment]
 
     user.bookmarks.find_or_create_by(bookmarkable: types.sample.all.sample)
@@ -83,7 +78,7 @@ end
 
 # tags
 Post.all.each do |post|
-  rand(3..5).times do
+  rand(1..3).times do
     post.tags.find_or_create_by(name: Faker::Lorem.sentence(word_count: 2))
   end
 end
