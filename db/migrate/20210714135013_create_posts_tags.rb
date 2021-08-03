d2c7dd62b477f237a9decd1530d9e6bffa5ddd0a
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 18:52:38 +0600

    updated timestamps in post tags migrations

--- db/migrate/20210714135013_create_posts_tags.rb
+++ db/migrate/20210714135013_create_posts_tags.rb
@@ -5,6 +5,7 @@ class CreatePostsTags < ActiveRecord::Migration[6.1]
     create_table :posts_tags, id: false do |t|
       t.belongs_to :post, null: false, foreign_key: true
       t.belongs_to :tag, null: false, foreign_key: true
+      t.timestamps
     end
   end
 end
