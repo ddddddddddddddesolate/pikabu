98ef013dcacbf0877aad6113c89028d5ae218af8
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 15:56:48 +0600

    added database cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 
+require "support/database_cleaner"
 require "support/factory_bot"
 require "faker"
 
@@ -19,7 +20,8 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.use_transactional_fixtures = false
 end
---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before :suite do
+    DatabaseCleaner[:active_record].clean_with :truncation, except: %w(ar_internal_metadata)
+  end
+
+  config.before :each do
+    DatabaseCleaner[:active_record].strategy = :transaction
+    DatabaseCleaner[:active_record].start
+  end
+
+  config.after :each do
+    DatabaseCleaner[:active_record].clean
+  end
+end
