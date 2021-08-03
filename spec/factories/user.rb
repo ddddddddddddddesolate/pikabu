d9ab9683e28073edd90295b9a84294a4e7ec89ba
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 10:44:37 +0600

    updated user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,7 @@ require "faker"
 
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.first_name }
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password }
   end
