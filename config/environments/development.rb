e533e8eeb4481b5783a2100ffe736399be0a4cf2
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-08-03 11:21:57 +0600

    added smtp configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "mail", port: 1025 }
+
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
